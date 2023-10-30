603ce2e8f3b84f838ae3285fda7bed13cb30c6a1
ksooh <1kimc13@gmail.com>
2023-10-30 20:56:17 +0900

    화면 UI 바꿈

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,12 @@
 // import 'package:google_mobile_ads/google_mobile_ads.dart';
-import 'package:capstone/main/homePage.dart';
 import 'package:capstone/mainPage.dart';
-import 'package:capstone/style.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'mainPage.dart';
-import 'package:get/get.dart';
 // import 'package:modu_tour/singPage.dart';
-import 'login.dart';
+// import 'login.dart';
 
-void main()async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  KakaoSdk.init(nativeAppKey: 'fd5d11f7f28cc940b19167e60572b61a');
+void main(){
+  // WidgetsFlutterBinding.ensureInitialized();
   // MobileAds.instance.initialize();
   runApp(MyApp());
 }
@@ -23,19 +16,22 @@ class MyApp extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
-    return GetMaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title:'에코투어',
-      theme: ThemeData(
-        //primarySwatch: Color.green,
-        fontFamily: 'Pretendard',
-      ),
-      /*initialRoute: '/',
-      routes:{
-        '/':(context)=>Login(),
-        //'/sign':(context) =>SignPage(),
-    },*/
+      title:'그린패스',
+      // theme: ThemeData(
+      //   colorScheme: ColorScheme.fromSwatch(
+      //     primarySwatch: Colors.blue, // 주요 색상 설정
+      //     accentColor: Colors.green, // 강조 색상 설정
+      //   ),// 강조 색상 (예: 버튼 클릭 시 등)
+      //   // 여기에 다른 테마 속성들을 추가할 수 있습니다.
+      // ),
       home:MainPage(),
     );
   }
 }
+
+
+
+
+
--- lib/mainPage.dart
+++ lib/mainPage.dart
@@ -1,127 +1,149 @@
-// import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'main/homePage.dart';
-import 'main/planPage.dart';
-import 'main/naturePage.dart';
 import 'style.dart';
+import 'package:capstone/3catagories/naturePage.dart';
+import 'package:capstone/3catagories/mapPage.dart';
+import 'package:capstone/3catagories/planPage.dart';
 
-class MainPage extends StatefulWidget{
-  // const MainPage({super.key});
+
+class MainPage extends StatefulWidget {
   @override
-  State<StatefulWidget> createState()=>_MainPage();
+  State<StatefulWidget> createState() => _MainPage();
 }
 
-class _MainPage extends State<MainPage> with SingleTickerProviderStateMixin{
-  TabController? controller;
-  // FirebaseDatabase? _database;
-  // DatabaseReference? reference;
-  // String _databaseURL='### 데이터 베이스 URL 넣기 ###';
-  // String? id;
 
-  @override
-  void initState(){
-    super.initState();
-    controller = TabController(length: 3, vsync: this);
-    // _database=FireBase(databaseURL:_databaseURL);
-    // reference = _database!.reference();
+class _MainPage extends State<MainPage> {
+  int _selectedIndex = 1; // Index of the selected tab
+
+  final List<Widget> _pages = [
+    MapPage(),
+    PlanPage(),
+    NaturePage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 
   @override
-  void dispose(){
-    controller!.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context){
-    // 로그인 할 때 전달 받은 아이디 값 저장.
-    // id 변수는 String형이므로 as String으로 형변환 하도록 함.
-    // 각 페이지로 이동할 때마다 이 값을 전달함.
-    // id = ModalRoute.of(context)!.settings.arguments as String?;
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        title: const Text(
-          '에코투어',
-          textAlign: TextAlign.center,
-          style: TextStyle(color: Colors.black,),
-        ),
-        centerTitle: true,
-        actions: [
-          IconButton(
-              onPressed: (){},
-              icon: const Icon(
-                  Icons.account_circle,
-                  color:Colors.black
-              )
-          ),
-        ],
-        bottom: PreferredSize(
-          preferredSize: Size.fromHeight(1), // 선의 높이를 조절하려면 이 값을 변경
-          child: Container(
-            decoration: BoxDecoration(
-              border: Border(
-                bottom: BorderSide(
-                  color: Colors.black, // 선의 색상을 설정
-                  width: 1.0, // 선의 두께를 설정
-                ),
-              ),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(60.0),
+        child: AppBar(
+          elevation: 1,
+          backgroundColor: Colors.white,
+          title: const Text(
+            '그린패스',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontFamily: "Lotte", fontSize: 25,
+              color: Colors.black,
             ),
+          ),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.account_circle, color: Colors.black),
+            ),
+          ],
+        ),
+      ),
+
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(16.0),
+          topRight: Radius.circular(16.0),
+        ),
+        child: BottomAppBar(
+          color: Colors.white, // 네비게이션 바의 배경색
+          shape: CircularNotchedRectangle(), // 테두리를 둥글게 만듭니다.
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        _selectedIndex = 0;
+                      });
+                    },
+                    child: Text(
+                      '관광지도',
+                      style: TextStyle(
+                        fontFamily: "Lotte",
+                        fontSize: 15,
+                        color: _selectedIndex == 0 ? AppColor.deepGreen : Colors.grey,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        _selectedIndex = 2;
+                      });
+                    },
+                    child: Text(
+                      '환경스토어',
+                      style: TextStyle(
+                        fontFamily: "Lotte",
+                        fontSize: 15,
+                        color: _selectedIndex == 2 ? AppColor.deepGreen : Colors.grey,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
 
 
-
-      body: TabBarView(
-        controller: controller,
-        children: <Widget>[
-          // TabBarView에 채울 위젯들
-          PlanPage(),
-          HomePage(),
-          NaturePage()
-        ],
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Container(
+        height: 70.0,
+        width: 70.0,
+        child: FittedBox(
+          child: FloatingActionButton(
+            backgroundColor: AppColor.lightGreen,
+            onPressed: () {
+              setState(() {
+                _selectedIndex = 1;
+              });
+            },
+            child: Column(
+                children: [
+                  Container(height: 17,),
+                  Icon(Icons.edit_calendar, color: Colors.black, size: 23,),
+                  // Container(height: 2,),
+                  // Text(
+                  //   "내 계획",
+                  //   style: TextStyle(
+                  //     // fontFamily: "Lotte",
+                  //     fontSize: 8,
+                  //     color: Colors.black,
+                  //   ),
+                  // ),
+                ]
+            ),
+          ),
+        ),
       ),
-      bottomNavigationBar: TabBar(
-        tabs: const <Tab>[
-          Tab(
-            icon: Icon(Icons.edit_calendar),
-            child: Text(
-              "내 계획",
-              style: TextStyle(
-                fontSize: 11.0, // 글씨 크기
-                fontFamily: 'YourFontFamily', // 원하는 글꼴 지정
-                fontWeight: FontWeight.bold, // 글꼴 두껍게 설정 (선택 사항)
-              ),
-            ),
-          ),
-          Tab(
-            icon: Icon(Icons.home),
-            child: Text(
-              "홈",
-              style: TextStyle(
-                fontSize: 11.0, // 글씨 크기
-                fontFamily: 'YourFontFamily', // 원하는 글꼴 지정
-                fontWeight: FontWeight.bold, // 글꼴 두껍게 설정 (선택 사항)
-              ),
-            ),
-          ),
-          Tab(
-            icon: Icon(Icons.public),
-            child: Text(
-              "환경스토어",
-              style: TextStyle(
-                fontSize: 11.0, // 글씨 크기
-                fontFamily: 'YourFontFamily', // 원하는 글꼴 지정
-                fontWeight: FontWeight.bold, // 글꼴 두껍게 설정 (선택 사항)
-              ),
-            ),
-          )
-        ],
-        labelColor:AppColor.newGreen,
-        indicatorColor: AppColor.deepGreen,
-        controller:controller,
-      ));
+
+    );
   }
 }
+
+
+
