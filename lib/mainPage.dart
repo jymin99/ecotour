aef3fc0020befff1e204fb6bd8c2c6a9be415009
ksooh <1kimc13@gmail.com>
2023-11-01 22:58:14 +0900

    dialog 달력 사이즈 조절중

--- lib/mainPage.dart
+++ lib/mainPage.dart
@@ -3,6 +3,8 @@ import 'style.dart';
 import 'package:capstone/3catagories/naturePage.dart';
 import 'package:capstone/3catagories/mapPage.dart';
 import 'package:capstone/3catagories/planPage.dart';
+import 'package:table_calendar/table_calendar.dart';
+
 
 
 class MainPage extends StatefulWidget {
@@ -13,6 +15,16 @@ class MainPage extends StatefulWidget {
 
 class _MainPage extends State<MainPage> {
   int _selectedIndex = 1; // Index of the selected tab
+  IconData _floatingButtonIcon = Icons.edit_calendar; // Default icon
+
+  DateTime selectedDay = DateTime(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+  DateTime focusedDay = DateTime.now();
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+
 
   final List<Widget> _pages = [
     MapPage(),
@@ -25,6 +37,153 @@ class _MainPage extends State<MainPage> {
       _selectedIndex = index;
     });
   }
+
+
+  void _showCustomDialog(BuildContext context) {
+    String title = ''; // 제목을 저장할 변수
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Center(
+          child: AlertDialog(
+            content: SingleChildScrollView(
+              child: Container( // 내용을 감싸는 컨테이너에 폭 설정
+                width: 800.0, // 원하는 폭 설정
+                height: 400.0,
+                child: Column(
+                  children: [
+                    TextField(
+                      onChanged: (String value) {
+                        title = value; // 입력된 제목을 변수에 저장
+                      },
+                      decoration: InputDecoration(
+                        labelText: '제목',
+                        prefixIcon: Icon(Icons.search, color: Colors.black), // 돋보기 아이콘 추가 및 색상 설정
+                        filled: true, // 배경색 적용
+                        fillColor: Colors.grey[200], // 배경색 설정
+                        border: OutlineInputBorder( // 텍스트 필드의 테두리 스타일 설정
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: BorderSide.none, // 테두리 없음
+                        ),
+                      ),
+                    ),
+
+                    // TextField(
+                    //   onChanged: (String value) {
+                    //     title = value; // 입력된 제목을 변수에 저장
+                    //   },
+                    //   decoration: InputDecoration(labelText: '제목'),
+                    // ),
+
+                    Container(
+                      margin: EdgeInsets.only(right: .0, left: 4.0, top:20.0),
+
+                      padding: EdgeInsets.all(5.0),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.grey,
+                          width: 2.0,
+                        ),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Column(
+                        children: [
+                          TableCalendar(
+                            formatAnimationDuration: Duration(milliseconds: 220),
+                            locale: 'ko_KR',
+                            firstDay: DateTime.utc(2021, 1, 1),
+                            lastDay: DateTime.utc(2030, 12, 31),
+                            focusedDay: focusedDay,
+                            calendarFormat: _calendarFormat,
+                            availableCalendarFormats: const {
+                              CalendarFormat.month: 'month',
+                              CalendarFormat.twoWeeks: 'week',
+                            },
+                            onFormatChanged: (format) {
+                              setState(() {
+                                _calendarFormat = format;
+                              });
+                            },
+                            onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
+                              // 선택된 날짜의 상태를 갱신합니다.
+                              setState((){
+                                this.selectedDay = selectedDay;
+                                this.focusedDay = selectedDay;
+                              });
+                            },
+                            selectedDayPredicate: (DateTime day) {
+                              // selectedDay 와 동일한 날짜의 모양을 바꿔줍니다.
+                              return isSameDay(selectedDay, day);
+                            },
+                            headerStyle: const HeaderStyle(
+                              headerPadding:
+                              EdgeInsets.symmetric(horizontal: 0.0, vertical: 0.0),
+                              titleCentered: true,
+                              titleTextStyle: TextStyle(
+                                fontSize: 13.0,
+                                color: Colors.black,
+                              ),
+                            ),
+
+                            calendarStyle: const CalendarStyle(
+                              // 이전 달 날짜의 스타일 설정
+                              outsideTextStyle: TextStyle(fontSize: 12, color: Colors.grey),
+                              // outsideDecoration: BoxDecoration(), // 이전 달 날짜의 배경 스타일
+
+                              // 오늘 날짜의 스타일 설정
+                              todayTextStyle: TextStyle(fontSize: 12, color: Colors.white),
+                              // todayDecoration: BoxDecoration(shape: BoxShape.circle, color: Colors.yellow), // 오늘 날짜의 배경 스타일
+
+                              defaultTextStyle: TextStyle(fontSize: 12.0), // 기본 날짜 숫자의 폰트 크기
+                              weekendTextStyle: TextStyle(fontSize: 12.0, color: Colors.red), // 주말의 날짜 숫자 폰트 크기와 색상
+                              // selectedDay 글자 조정
+                              selectedTextStyle : TextStyle(
+                                color: Colors.black,
+                                fontSize: 12.0,
+                              ),
+                              // selectedDay 모양 조정
+                              selectedDecoration : BoxDecoration(
+                                color: AppColor.yellowGreen,
+                                shape: BoxShape.rectangle,
+                              ),
+                              cellMargin: EdgeInsets.all(0.0), // 셀 주위 여백 설정
+                              cellPadding: EdgeInsets.all(0.0), // 셀 내부 여백 설정
+                            ),
+
+                            daysOfWeekStyle: const DaysOfWeekStyle(
+                              // 요일 텍스트 스타일
+                              weekdayStyle: TextStyle(fontSize: 13.0), // 평일 요일의 스타일
+                              weekendStyle: TextStyle(fontSize: 13.0), // 주말 요일의 스타일
+                            ),
+
+                          ),
+                          // 여기에 다른 위젯을 추가할 수 있음
+                        ],
+                      ),
+                    ),
+
+
+                  ],
+                ),
+              ),
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(); // 다이얼로그 닫기
+                },
+                child: Text('Close'),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -71,6 +230,7 @@ class _MainPage extends State<MainPage> {
                     onPressed: () {
                       setState(() {
                         _selectedIndex = 0;
+                        _floatingButtonIcon = Icons.edit_calendar;
                       });
                     },
                     child: Text(
@@ -91,6 +251,7 @@ class _MainPage extends State<MainPage> {
                     onPressed: () {
                       setState(() {
                         _selectedIndex = 2;
+                        _floatingButtonIcon = Icons.edit_calendar;
                       });
                     },
                     child: Text(
@@ -109,7 +270,8 @@ class _MainPage extends State<MainPage> {
         ),
       ),
 
-
+      // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      // floatingActionButton: _selectedIndex ==1 ? Container()
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: Container(
         height: 70.0,
@@ -118,10 +280,11 @@ class _MainPage extends State<MainPage> {
           child: FloatingActionButton(
             backgroundColor: AppColor.lightGreen,
             onPressed: () {
+              _floatingButtonIcon = _selectedIndex == 1 ? Icons.edit_calendar : Icons.add;
               if (_selectedIndex == 1) {
                 // 현재 페이지가 인덱스 1 (PlanPage)인 경우
                 // 정보 입력 및 페이지 업데이트 동작 수행
-                _showInputDialog(context); // 정보 입력 다이얼로그 표시
+                _showCustomDialog(context); // 정보 입력 다이얼로그 표시
 
               } else {
                 setState(() {
@@ -132,16 +295,7 @@ class _MainPage extends State<MainPage> {
             child: Column(
                 children: [
                   Container(height: 17,),
-                  Icon(Icons.edit_calendar, color: Colors.black, size: 23,),
-                  // Container(height: 2,),
-                  // Text(
-                  //   "내 계획",
-                  //   style: TextStyle(
-                  //     // fontFamily: "Lotte",
-                  //     fontSize: 8,
-                  //     color: Colors.black,
-                  //   ),
-                  // ),
+                  Icon(_floatingButtonIcon, color: Colors.black, size: 23,),
                 ]
             ),
           ),
@@ -154,28 +308,44 @@ class _MainPage extends State<MainPage> {
 
 
 
+//
+// void _showInputDialog(BuildContext context) {
+//   String title = ''; // 제목을 저장할 변수
+//   DateTime selectedDate = DateTime.now(); // 달력 기간을 저장할 변수
+//
+//   showDialog(
+//     context: context,
+//     builder: (BuildContext context) {
+//       return Center(
+//         child: Container(
+//           height: 500,
+//           width: 600,
+//           child: AlertDialog(
+//             title: Text('계획 추가'),
+//             // insetPadding: EdgeInsets.symmetric(vertical: 80.0, horizontal: 0.0),
+//             content: Column(
+//               children: [
+//                 TextField(
+//                   onChanged: (String value) {
+//                     title = value; // 입력된 제목을 변수에 저장
+//                   },
+//                   decoration: InputDecoration(labelText: '제목'),
+//                 ),
+//               ],
+//             ),
+//             actions: [
+//               TextButton(
+//                 onPressed: () {
+//                   Navigator.of(context).pop(); // 다이얼로그 닫기
+//                 },
+//                 child: Text('Close'),
+//               ),
+//             ],
+//           ),
+//         ),
+//       );
+//     },
+//   );
+// }
 
-void _showInputDialog(BuildContext context) {
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: Text('Enter Information'),
-        content: TextField(
-          onChanged: (String value) {
-            // 입력된 정보 처리
-            // 페이지 업데이트 등을 수행
-          },
-        ),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop(); // 다이얼로그 닫기
-            },
-            child: Text('Close'),
-          ),
-        ],
-      );
-    },
-  );
-}
+
